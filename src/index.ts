e0c2627cc05ebf7ee8dde0e77b91638b4a9805b1
handriko-code <handrikocode@gmail.com>
2025-04-08 22:18:27 +0700

    project on progress

---
+++ src/index.ts
@@ -0,0 +1,75 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+import fs from 'fs';
+import { readDataJSON } from './utils/read.data.json';
+import { writeDataJSON } from './utils/write.data.json';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+    res.status(200).json('Welcome to Express API');
+  });
+
+//1. Create a Purchase Order
+app.post('/purchaseOrders', (req: Request, res: Response) => {
+    try {
+        const {itemName, category, quantity, supplier, status} = req.body;
+  
+      const data = readDataJSON();
+        data.purchaseOrders.push({
+        id: data.purchaseOrders[data.purchaseOrders.length - 1].id + 1,
+        itemName,
+        category,
+        quantity,
+        supplier,
+        status,
+      });
+   
+     writeDataJSON(data);
+  
+      res.status(201).json({
+        success: true,
+        message: 'Purchase order created successfully',
+        data: { itemName, category, quantity, supplier, status },
+      });
+    } catch (error: any) {
+      res.status(500).json({
+        success: false,
+        message: error.message,
+        data: null,
+      });
+    }
+  });
+
+//2. Read All Purchase Orders
+app.get('/purchaseOrders', (req: Request, res: Response) => {
+  try {
+    const data = readDataJSON();
+  
+    res.status(201).json({
+      success: true,
+      message: 'Get Purchase order successfully',
+      data: data.purchaseOrders,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+});
+
+//3. Read a Purchase Order by ID
+
+// Postman: http://localhost:5000/purchaseOrders
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+  
