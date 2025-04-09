5757c2e727d1734dab1422d2150267b181020785
handriko-code <handrikocode@gmail.com>
2025-04-09 19:57:29 +0700

    Project Case Study 20240409

--- src/index.ts
+++ src/index.ts
@@ -67,6 +67,101 @@ app.get('/purchaseOrders', (req: Request, res: Response) => {
 });
 
 //3. Read a Purchase Order by ID
+app.get('/purchaseOrders/:id', (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { itemName, category, quantity, supplier, status } = req.body;
+
+    const data = readDataJSON();
+
+    const findIndex = data.purchaseOrders.findIndex(
+      (item: any) => item.id === Number(id)
+    );
+
+    if (findIndex === -1) throw new Error(`Purchase Order with Id ${id} Not Found`);
+
+    res.status(201).json({
+      success: true,
+      message: 'Get Purchase order successfully',
+      data: data.purchaseOrders,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+});
+
+//4. Update  a Purchase Order
+app.put('/purchaseOrders/:id', (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const {itemName, category, quantity, supplier, status} = req.body;
+
+    const data = readDataJSON();
+    const findIndex = data.purchaseOrders.findIndex(
+      (item: any) => item.id === Number(id)
+    );
+
+    if (findIndex === -1) throw new Error(`Purchase Order with Id ${id} Not Found`);
+
+    data.purchaseOrders[findIndex] = {
+      ...data.purchaseOrders[findIndex],
+      itemName,
+      category,
+      quantity,
+      supplier,
+      status,
+    };
+
+    writeDataJSON(data);
+
+    res.status(200).json({
+      success: true,
+      message: `Update Purchase Order with Id ${id} Success`,
+      data: { itemName, category, quantity, supplier, status },
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+});
+
+//5. Delete a Purchase Order
+app.delete('/purchaseOrders/:id', (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const data = readDataJSON();
+
+    const findIndex = data.purchaseOrders.findIndex(
+      (item: any) => item.id == Number(id)
+    );
+
+    if (findIndex === -1) throw new Error(`Purchase Order with Id ${id} Not Found`);
+
+    data.purchaseOrders.splice(findIndex, 1);
+
+    writeDataJSON(data);
+
+    res.status(200).json({
+      success: true,
+      message: `Delete Puchase Order with Id ${id} Success`,
+      data: null,
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+});
+
 
 // Postman: http://localhost:5000/purchaseOrders
 app.listen(port, () => {
